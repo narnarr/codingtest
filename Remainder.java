6b3cd9ef96d678a902f35bc39254fd1da8f38a65
narnarr <67263648+narnarr@users.noreply.github.com>
2021-07-20 02:30:14 +0900

    Create Remainder.java

---
+++ Remainder.java
@@ -0,0 +1,30 @@
+package level5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Remainder {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[10];
+		
+		for (int i=0; i<10; i++) {
+			arr[i] = Integer.parseInt(br.readLine())%42;
+		}
+		
+		int cnt = 10;
+		
+		for (int i=0; i<10; i++) {
+			for (int j=i+1; j<10; j++) {
+				if (arr[i]==arr[j]) {
+					cnt--;
+					break;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
