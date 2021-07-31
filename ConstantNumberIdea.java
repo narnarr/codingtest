a903cd8dcbb317ce973b62bf7dd5f2511de5c39a
narnarr <67263648+narnarr@users.noreply.github.com>
2021-08-01 05:43:44 +0900

    Create ConstantNumberIdea.java

---
+++ ConstantNumberIdea.java
@@ -0,0 +1,20 @@
+package level7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ConstantNumber {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		System.out.println((a>b)?a:b);
+	}
+}
