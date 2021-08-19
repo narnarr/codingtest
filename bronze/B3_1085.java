ac7beb774f6bc87f4ee052f989749c6499a44b52
narnarr <67263648+narnarr@users.noreply.github.com>
2021-08-20 05:01:46 +0900

    Create B3_1085.java

---
+++ bronze/B3_1085.java
@@ -0,0 +1,27 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Escape from a rectangle
+public class B3_1085 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken()); //x
+		int h = Integer.parseInt(st.nextToken()); //y
+
+		int xMin = Math.min(x, w-x);
+		int yMin = Math.min(y, h-y);
+		
+		System.out.println(Math.min(xMin, yMin));
+	}
+
+}
